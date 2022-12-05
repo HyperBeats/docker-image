184d36f0f01e1a045873f00db1a072c7a99b8120
HyperBeats <kiran26200@gmail.com>
2022-12-05 22:23:35 +0100

    update

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -16,6 +16,18 @@ node -v
 # shellcheck disable=SC2086
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
+echo -e ""
+echo -e ""
+echo -e "${CYAN}
+    ____                            __  __           __  _            
+   / __ \____ _      __            / / / /___  _____/ /_(_)___  ____ _
+  / /_/ / __ \ | /| / /  ______   / /_/ / __ \/ ___/ __/ / __ \/ __ `/
+ / _, _/ /_/ / |/ |/ /  /_____/  / __  / /_/ (__  ) /_/ / / / / /_/ / 
+/_/ |_|\____/|__/|__/           /_/ /_/\____/____/\__/_/_/ /_/\__, /  
+                                                             /____/   
+${RESET_COLOR}"
+
+
 
 # Run the Server
 # shellcheck disable=SC2086
