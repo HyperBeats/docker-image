1b9359f0f94873a981e43036a88b0602ef6eed5f
HyperBeats <kiran26200@gmail.com>
2022-12-26 16:35:49 +0100

    edits

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd /home/container || exit 1
 
-# Configure colors
-CYAN='\033[0;36m'
-RESET_COLOR='\033[0m'
+#!/bin/bash
+cd /home/container
 
-# Set environment variable that holds the Internal Docker IP
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
@@ -13,12 +12,8 @@ export INTERNAL_IP
 node -v
 
 # Replace Startup Variables
-# shellcheck disable=SC2086
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
-echo -e "${CYAN}⟳${RESET_COLOR} Starting NodeJs..."
-echo -e "${GREEN}✓${RESET_COLOR} Successfully started"
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-# shellcheck disable=SC2086
 eval ${MODIFIED_STARTUP}
