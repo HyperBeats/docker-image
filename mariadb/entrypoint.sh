2fc41f8e557b5cdb547d2b27c59cb11e734a4150
HyperBeats <kiran26200@gmail.com>
2023-06-16 00:15:31 +0200

    Update entrypoint.sh

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd /home/container
 
+# Configure colors
+CYAN='\033[0;36m'
+RESET_COLOR='\033[0m'
+GREEN='\033[0;32m'
+
+
+
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
@@ -8,6 +15,24 @@ export INTERNAL_IP
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "   ___  ____ _      __    __ ______  _______________  _______"
+echo "  / _ \/ __ \ | /| / /___/ // / __ \/ __/_  __/  _/ |/ / ___/"
+echo " / , _/ /_/ / |/ |/ /___/ _  / /_/ /\ \  / / _/ //    / (_ / "
+echo "/_/|_|\____/|__/|__/   /_//_/\____/___/ /_/ /___/_/|_/\___/  "                                                             
+echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
+echo -e "${CYAN}⟳${RESET_COLOR} Starting MariaDB..."
+echo -e "${GREEN}✓${RESET_COLOR} Successfully started"
+
+read -r username < username.txt && read -r password < password.txt
+
+sql_commands=$(cat <<EOF
+CREATE USER '$username'@'%' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON *.* TO '$username'@'%';
+GRANT ALL PRIVILEGES ON proddb.* TO '$username'@'%';
+FLUSH PRIVILEGES;
+EOF
+)
+echo "$sql_commands" | mysql -u root -p
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
