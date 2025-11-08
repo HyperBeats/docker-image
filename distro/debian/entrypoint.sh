dbfb446f1809af7b6dc68d2ff910e7ab1f4388c0
HyperBeats <45314346+HyperBeats@users.noreply.github.com>
2025-11-08 18:21:16 +0100

    Update entrypoint.sh

--- distro/debian/entrypoint.sh
+++ distro/debian/entrypoint.sh
@@ -5,24 +5,21 @@ CYAN='\033[0;36m'
 RESET_COLOR='\033[0m'
 GREEN='\033[0;32m'
 
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-update-ca-certificates --fresh >/dev/null 2>&1
-export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-
+# affiche l'heure
 date
-
-MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "   ___  ____ _      __    __ ______  _______________  _______"
 echo "  / _ \/ __ \ | /| / /___/ // / __ \/ __/_  __/  _/ |/ / ___/"
 echo " / , _/ /_/ / |/ |/ /___/ _  / /_/ /\ \  / / _/ //    / (_ / "
-echo "/_/|_|\____/|__/|__/   /_//_/\____/___/ /_/ /___/_/|_/\___/  "
-
-echo -e "${CYAN}STARTUP /home/container:${RESET_COLOR} ${MODIFIED_STARTUP}"
+echo "/_/|_|\____/|__/|__/   /_//_/\____/___/ /_/ /___/_/|_/\___/  "                                                             
+echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
 echo -e "${CYAN}⟳${RESET_COLOR} Starting Container..."
 echo -e "${GREEN}✓${RESET_COLOR} Successfully started"
 
-eval "${MODIFIED_STARTUP}"
+# Run the Server
+eval ${MODIFIED_STARTUP}
