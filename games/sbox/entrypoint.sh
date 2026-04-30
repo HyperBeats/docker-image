1726d2f4ad697f3f2778c124910ee088a4a8cbd5
HyperBeats <45314346+HyperBeats@users.noreply.github.com>
2026-05-01 01:04:57 +0200

    fix

--- games/sbox/entrypoint.sh
+++ games/sbox/entrypoint.sh
@@ -1,46 +1,11 @@
 #!/bin/bash
+set -u
+cd /home/container
 
-#
-# Copyright (c) 2021 Matthew Penner
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-
-# Give everything time to initialize for preventing SteamCMD deadlock
-sleep 1
-
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
 
 CYAN='\033[0;36m'
 RESET_COLOR='\033[0m'
 GREEN='\033[0;32m'
-
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
-#!/bin/bash
-set -u
-cd /home/container
-
 # ---- Banner ----
 echo "Running on Debian $(cat /etc/debian_version 2>/dev/null || echo unknown)"
 echo "Current timezone: $(cat /etc/timezone 2>/dev/null || echo UTC)"
@@ -103,6 +68,8 @@ if [ "${XVFB}" = "1" ]; then
     fi
 fi
 
+# ---- 4. Execution ----
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 echo "   ___  ____ _      __    __ ______  _______________  _______"
 echo "  / _ \/ __ \ | /| / /___/ // / __ \/ __/_  __/  _/ |/ / ___/"
@@ -112,5 +79,4 @@ echo -e "${CYAN}STARTUP /home/container: ${MODIFIED_STARTUP} ${RESET_COLOR}"
 echo -e "${CYAN}⟳${RESET_COLOR} Starting Gmod..."
 echo -e "${GREEN}✓${RESET_COLOR} Successfully started"
 
-# shellcheck disable=SC2086
-eval exec "${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
